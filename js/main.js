b54f9f27c611da594870f02a5e48f9330528f629
Erik van `t Wout <erik@ewnetworks.eu>
2018-02-10 23:49:46 +0100

    * New stratum port and increase vardiff diff limit

--- js/main.js
+++ js/main.js
@@ -478,6 +478,7 @@ var init = function () {
     $('#stratumurl2').text('stratum+tcp://freshgarlicblocks.net:3333');
     $('#stratumurl3').text('stratum+tcp://freshgarlicblocks.net:3334');
     $('#stratumurl4').text('stratum+tcp://freshgarlicblocks.net:3335');
+    $('#stratumurl5').text('stratum+tcp://freshgarlicblocks.net:3336');
 
     var eventSource = new EventSource('/api/getinfo');
 
